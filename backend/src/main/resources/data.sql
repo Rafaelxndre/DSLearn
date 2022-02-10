5b057bc8c77aefcdb0c88224a6f27fd63c4bb1f3
Rafaelxndre <rafaelxndre@gmail.com>
2022-02-09 22:12:09 -0300

    Offer

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -15,3 +15,5 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
 
 INSERT INTO tb_course (name, img_uri, img_gray_uri) VALUES ('Bootcamp HTML', 'https://techvccloud.mediacdn.vn/zoom/600_315/2019/8/8/88b-15652618173341842809607-crop-1565261823940958680635.png', 'https://jornaldematogrosso.com.br/hf-conteudo/uploads/posts/2022/01/66976_45d7e8588c26518826afd6404c1a9613.jpg');
 
+INSERT INTO tb_offer (edition, star_Moment, end_Moment, course_id) VALUES ('1.0', TIMESTAMP WITH TIME ZONE '2020-07-21T20:59:19Z', TIMESTAMP WITH TIME ZONE '2020-07-21T20:59:19Z', 1);
+INSERT INTO tb_offer (edition, star_Moment, end_Moment, course_id) VALUES ('2.0', TIMESTAMP WITH TIME ZONE '2020-07-21T20:59:19Z', TIMESTAMP WITH TIME ZONE '2020-07-21T20:59:19Z', 1);
